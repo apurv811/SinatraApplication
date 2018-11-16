741322eaaa8512a65e6c0e87dde64cc37dd1f84c
apurv811 <apurv811@gmail.com>
2018-11-15 23:03:54 -0800

    Change in redirect

--- student.rb
+++ student.rb
@@ -68,7 +68,7 @@ end
 
 #Route for the new student form
 get '/students/new' do
-  halt(401, 'Not Authorized, Please go back and login') unless session[:admin]
+  redirect to('/login') unless session[:admin]
   @student = Student.new
   erb :new_student
 end
